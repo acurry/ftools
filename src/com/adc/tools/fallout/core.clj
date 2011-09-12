29353da4ad163827cad24be8fc33bb630b5d7920
acurry <acurry@texeltek.com>
2011-09-11 22:03:06 -0400

    First commit of com.adc.tools.fallout

---
+++ src/com/adc/tools/fallout/core.clj
@@ -0,0 +1,1 @@
+(ns com.adc.tools.fallout.core)
---
+++ test/com/adc/tools/fallout/test/core.clj
@@ -0,0 +1,6 @@
+(ns com.adc.tools.fallout.test.core
+  (:use [com.adc.tools.fallout.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
